2696e50dfa824c96f54439cf880cfdfdf94a197f
Studycode001 <dangrealok@gmail.com>
2024-06-06 12:02:57 +0530

    new models created

---
+++ src/models/comment.model..js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+
+const commentSchema = new Schema({
+
+    content: {
+        type: String,
+        reqiured: true
+    },
+
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    
+    video: {
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    }
+
+}, {timestamps: true})
+
+export const Comment = mongoose.model("Comment", commentSchema)
